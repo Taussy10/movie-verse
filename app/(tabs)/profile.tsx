23cdf00144cd52109cf8039059f6e4d95b720a0e
Tausif <tausif00100@gmail.com>
2025-04-28 19:36:04 +0530

    added logo and splash-screen icon

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+   <SafeAreaView
+     className=' flex-1 px-4'
+ 
+     >
+       <Text>Home</Text>
+     </SafeAreaView>
   )
 }
 
